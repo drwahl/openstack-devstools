6c1e01340c13026942fc869786948f4a81940e37
David Wahlstrom <david.wahlstrom@dreamhost.com>
2015-03-04 07:33:41 -0800

    Initial commit of cinder_cli.py
    This is the initial commit of cinder_cli.py.  This script will drop the
    user into an interactive shell with an authenticated cinder client
    (example: cclient.volumes.list() ).

---
+++ python/cinder_cli.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import code
+import readline
+import rlcompleter
+import os
+from cinderclient.v1 import client
+import openstack_variables
+
+osvars = openstack_variables.get()
+
+cclient = client.Client(
+    osvars['OS_USERNAME'],
+    osvars['OS_PASSWORD'],
+    project_id=osvars['OS_TENANT_NAME'],
+    auth_url=osvars['OS_AUTH_URL'],
+)
+
+print("""
+#################################################
+# Nova API access available through 'mclient.*' #
+# example: cclient.volumes.list()               #
+#################################################
+""")
+
+vars = globals()
+vars.update(locals())
+readline.set_completer(rlcompleter.Completer(vars).complete)
+readline.parse_and_bind("tab: complete")
+shell = code.InteractiveConsole(vars)
+shell.interact()
