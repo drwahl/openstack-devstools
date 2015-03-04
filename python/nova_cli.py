19208f6d11e0f8e7244b369420b36dc41df62a8e
David Wahlstrom <david.wahlstrom@dreamhost.com>
2015-03-04 07:16:37 -0800

    Initial commit of nova_cli.py
    This patch includes the initial commit of nova_cli.py.  Running this
    script will drop the user into an interactive python terminal with
    access to an authenticated nova client (example: nclient.flavors.list()
    ).

---
+++ python/nova_cli.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import code
+import readline
+import rlcompleter
+import os
+from novaclient.v2 import client
+import openstack_variables
+
+osvars = openstack_variables.get()
+
+nclient = client.Client(
+    osvars['OS_USERNAME'],
+    osvars['OS_PASSWORD'],
+    osvars['OS_TENANT_NAME'],
+    osvars['OS_AUTH_URL'],
+    no_cache=True
+)
+
+print("""
+#################################################
+# Nova API access available through 'mclient.*' #
+# example: nclient.flavors.list()               #
+#################################################
+""")
+
+vars = globals()
+vars.update(locals())
+readline.set_completer(rlcompleter.Completer(vars).complete)
+readline.parse_and_bind("tab: complete")
+shell = code.InteractiveConsole(vars)
+shell.interact()
